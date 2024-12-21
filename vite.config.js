598afa3c2b552a7ecf997d552796644bea69f1c8
caspar5911 <caspar95galaxy@gmail.com>
2024-12-22 00:08:10 +0800

    fix: cicd base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Santa_Rush/', // Update this with your repository name
+  // other configurations...
   plugins: [react()],
 })
